e6051909aa451505aca66bb1654a85cada832242
Christopher Allen Henyard <chenyard@gmail.com>
2026-03-10 17:02:12 -0700

    Mcp csharp code tool

---
+++ src/MCPServer/McpTools/CSharpCodeTools.cs
@@ -0,0 +1,18 @@
+﻿using McpServer.Models;
+using McpServer.Services;
+using ModelContextProtocol.Server;
+using System.ComponentModel;
+using System.Text.Json;
+
+namespace McpServer.McpTools;
+
+[McpServerToolType]
+public static class CSharpCodeTools
+{
+    [McpServerTool, Description("Find C# code from a local codebase.")]
+    public static async Task<string> GetMonkey(CSharpCodeService codeService, [Description("The user's request")] AskRequest request)
+    {
+        var askRespone = await codeService.Ask(request);
+        return JsonSerializer.Serialize(askRespone);
+    }
+}
